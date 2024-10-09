fcd4b29f4bfdc61f658e658df8494584df67b55e
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-10-09 11:35:22 +0530

    Add userRepositories Check the by email

---
+++ src/repositories/userRepositories.js
@@ -0,0 +1,23 @@
+import User from "../schema/user";
+
+
+
+export const findUserByEmail = async (email) => {
+    try {
+        const user = await User.findOne( {email} );
+        return user;
+    } catch (error) {
+        console.log("email not found", error);
+        
+    }
+}
+
+export const findAllUsers = async () => {
+    try {
+        const users = await User.find();
+        return users;
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
