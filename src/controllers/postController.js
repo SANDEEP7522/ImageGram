730690c4e8936b11879689b144830e93dc3244f4
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-10-09 16:31:25 +0530

    Add controllers & also try middleware add in comment

---
+++ src/controllers/postController.js
@@ -0,0 +1,5 @@
+export async function creatPost(req, res) {
+    // call the service layer function
+   return res.json({ message: 'Post created successfully' });
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 import express from 'express';
 import connectDB from './config/dbConfig.js';
+import { creatPost } from './controllers/postController.js';
 
 const PORT = 3000;
 const app = express();
 
-// app.get('/', (req, res) => {
-//     return res.send('Home');
-// })
-
 app.get('/ping', (req, res) => {
     return res.json({ message: 'Pong' })
  })
-// app.get('/hello', (req, res) => {
-//    return res.json({ message: 'hello I am here' })
-// })
+
+// middlewere
+ 
+//  function m1(req, res, next) {
+//     console.log('m1');
+//     next();
+//  }
+//  function m2(req, res, next) {
+//     console.log('m2');
+//     next();
+//  }
+//  function m3(req, res, next) {
+//     console.log('m3');
+//     next();
+//  }            
+//                          [m1, m2, m3],
+ app.post('/posts',                             creatPost);
+ 
 app.listen(PORT, () => {
     console.log(`Server is running port nnumber: ${ PORT }`);
     connectDB(); 
