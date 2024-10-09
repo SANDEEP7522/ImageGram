30672d9eba4f3008587fb64bdae208c265529b9f
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-10-09 15:34:23 +0530

    Add AWS function without Key

---
+++ src/services/postService.js
@@ -0,0 +1,6 @@
+export const createPost = async (createPostObject) => {
+     // Take the image of Post and Upload on aws or claudinary
+     // get the url of inage frome the asw or cloudinary response
+     // create a post with the caption and image in the DB using the repository
+     // Return the post object 
+}
