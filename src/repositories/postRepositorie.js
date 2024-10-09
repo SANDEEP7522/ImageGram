9cbe82e9c218862a00f6c5ca57fd4c68fb14b2d2
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-10-09 12:12:10 +0530

    Add functio  postRepositorie creat, find, del, upd

---
+++ src/repositories/postRepositorie.js
@@ -0,0 +1,49 @@
+import Post from "../schema/post";
+
+export const createPost = async (caption, image, user) => {
+    try {
+        const newPost = await Post.create( {caption, image, user} );
+        return newPost;
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export const findAllPost = async () => {
+    try {
+        const posts = await Post.find();
+        return posts;
+    } catch (error) {
+        console.log(error);
+    }
+}
+export const findPostById = async (id) => {
+    try {
+        const post = await Post.findById(id);
+        return post;
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export const deletePostBYId = async (id) => {
+    try {
+        const post = await Post.deletePostBYId(id);
+        return post;
+    } catch (error) {
+        console.log(error);       
+    }
+}
+export const updatePostById = async (id) => {
+    try {
+        const post = await Post.updatePostById(id);
+        return post;
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+
